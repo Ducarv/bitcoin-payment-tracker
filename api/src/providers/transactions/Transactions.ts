0d80eb9cb0c07ac573b01a5971999e8531c73b15
ducarv <educarvalho009@gmail.com>
2025-03-19 14:57:56 -0400

    refact: add confirmations and status

--- api/src/providers/transactions/Transactions.ts
+++ api/src/providers/transactions/Transactions.ts
@@ -4,21 +4,36 @@ import { fetchTransactionsByAddress, latestBlock } from '../Blockchain/api';
 export class TransactionsProvider {
 
     public async getTransactionsByAddress(address: string): Promise<ITransactions[]> {
-        const rawTransactions = await fetchTransactionsByAddress(address) as { data: { txs: any[] } };
+        const addressInfos = await fetchTransactionsByAddress(address) as { data: { txs: any[] } };
         const latestBlockData = await latestBlock() as { data: { height: number } };
+        
 
         const transactions: ITransactions[] = [];
 
-        rawTransactions.data.txs.forEach((transaction: any) => {
+        addressInfos.data.txs.forEach((transaction: any) => {
             const transactionObj: ITransactions = {
                 hash: transaction.hash,
                 value: transaction.result / 100_000_000,
                 timestamp: transaction.time,
-                //confirmations: latestBlockData.data.height - transaction.block_height
+                confirmations: latestBlockData.data.height - transaction.block_height + 1,
+                status: this.getTransactionStatus(transaction, address)
             };
             transactions.push(transactionObj);
         });
 
         return transactions;
+    }
+
+    private getTransactionStatus(transaction: any, address: string): "sent" | "recieved" | "both" {
+        const isSent = transaction.inputs.some((input: any) => input.prev_out.addr === address);
+        const isRecieved = transaction.out.some((output: any) => output.addr === address);
+
+        if (isSent && isRecieved) {
+            return "both";
+        } else if (isSent) {
+            return "sent";
+        } else {
+            return "recieved";
+        }
     }
 }
--- api/src/types/ITransactions.ts
+++ api/src/types/ITransactions.ts
@@ -2,5 +2,6 @@ export interface ITransactions {
     hash: string;
     value: number;
     timestamp: number;
-    //confirmations: number;
+    confirmations: number;
+    status: "recieved" | "sent" | "both";
 }
